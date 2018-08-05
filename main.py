477470093141d07db488f1d187a4c09108ec3cfe
Rodolfo De Nadai <rdenadai@gmail.com>
2018-08-05 17:46:11 -0300

    Small corrections

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import sys, pygame
 from pygame.locals import *
 import numpy as np
+from objects.configs import FPS, BLACK, WHITE, CRIMSON, GREEN, KEY, SNAKE_SIZE, APPLE_SIZE
+from objects.classes import Snake, Apple
 
 
 # obj = np.dtype([
@@ -13,72 +15,116 @@ import numpy as np
 #player = np.array([(0.1, 10, 10)], dtype=obj)
 
 
-class Player():
+def check_collision(objA, objB, objA_size=SNAKE_SIZE, objB_size=APPLE_SIZE):
+    if(
+        objA.x < objB.x + objB_size and
+        objA.x + objA_size > objB.x and
+        objA.y < objB.y + objB_size and
+        objA.y + objA_size > objB.y
+    ):
+        return True
+    return False
 
-    def __init__(self):
-        self.speed = 0.1
-        self.size_x = 10
-        self.size_y = 10
 
-    def draw(self, x, y):
-        pygame.draw.rect(screen, white, (x, y, 10, 10))
+def check_cash(snake):
+    counter = 1
+    stack = snake.stack
+    while counter < len(stack) - 1:
+        if check_collision(stack[0], stack[counter], SNAKE_SIZE, SNAKE_SIZE):
+            return True
+        counter += 1
+    return False
 
 
-class Food():
+def draw_object(scr, color, position):
+    pygame.draw.rect(scr, color, position)
 
-    def __init__(self):
-        self.pos_x = np.random.randint(0, width - 10)
-        self.pos_y = np.random.randint(0, height - 10)
 
-    def draw(self):
-        print(self.pos_x, self.pos_y)
-        pygame.draw.rect(screen, crimson, (self.pos_x, self.pos_y, 10, 10))
+def reload_apple():
+    apple_x = np.random.choice(np.arange(10, width - 10, 10))
+    apple_y = np.random.choice(np.arange(10, height - 10, 10))
+    return Apple(apple_x, apple_y, CRIMSON)
+
+
+def start_game():
+    score = 0
+    # Create the player
+    x = np.random.choice(np.arange(80, width - 80, 10))
+    y = np.random.choice(np.arange(80, height - 80, 10))
+    snake = Snake(x, y, GREEN, WHITE)
+    # Start food?
+    apple = reload_apple()
+    return score, snake, apple
 
 
 if __name__ == '__main__':
     pygame.init()
-
-    # Create the player
-    player = Player()
-    # Start food?
-    food = None
+    stop_game = False
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 20)
 
     size = width, height = 640, 480
-    speed = [2, 2]
-    black = (0, 0, 0)
-    crimson = (220, 20, 60)
-    white = (255, 255, 255)
 
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Paint")
+    screen = pygame.display.set_mode(size, pygame.HWSURFACE)
+    pygame.display.set_caption("Snake Plissken")
 
-    # Let's center the player
-    x = width / 2 + player.size_x
-    y = height / 2 + player.size_y
+    score, snake, apple = start_game()
     # Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        if stop_game:
+            score, snake, apple = start_game()
+            stop_game = False
+
         # Key movement
         pressed = pygame.key.get_pressed()
-        if pressed[K_UP]: y -= player.speed
-        if pressed[K_DOWN]: y += player.speed
-        if pressed[K_LEFT]: x -= player.speed
-        if pressed[K_RIGHT]: x += player.speed
-        # Border
-        if x <= 0: x = 0
-        if x >= width-10: x = width - player.size_x
-        if y <= 0: y = 0
-        if y >= height-10: y = height - player.size_y
+        if pressed[K_UP]: snake.head().direction = KEY['UP']
+        if pressed[K_DOWN]: snake.head().direction = KEY['DOWN']
+        if pressed[K_LEFT]: snake.head().direction = KEY['LEFT']
+        if pressed[K_RIGHT]: snake.head().direction = KEY['RIGHT']
+
+        # Check limits ! Border
+        boundary_hit = False
+        if snake.head().x <= 0: boundary_hit = True
+        if snake.head().x >= width: boundary_hit = True
+        if snake.head().y <= 0: boundary_hit = True
+        if snake.head().y >= height-10: boundary_hit = True
+        if boundary_hit:
+            score -= 1
+            stop_game = True
+
+        # Snake crash to its tail
+        if check_cash(snake):
+            score -= 1
+            stop_game = True
 
         # Clean screen
-        screen.fill(black)
-        if not food:
-            food = Food()
-            food.draw()
+        screen.fill(BLACK)
 
-        # Draw player
-        player.draw(x, y)
+        # Draw appple
+        if not apple:
+            apple = reload_apple()
+        draw_object(screen, apple.color, apple.position)
+
+        # Draw snake
+        snake.move()
+        for segment in snake.stack:
+            draw_object(screen, segment.color, (segment.x, segment.y) + segment.size)
+
+        # Check collision between snake and apple
+        if check_collision(snake.head(), apple):
+            apple = None
+            score += 1
+            snake.grow()
+
+        # Print on the screen the score
+        str_score = font.render(f'score: {score}', True, WHITE)
+        screen.blit(str_score, (5, 5))
+
+        # Routines
+        clock.tick(FPS)
+        pygame.display.flip()
         pygame.display.update()
---
+++ objects/classes.py
@@ -0,0 +1,68 @@
+from .configs import SNAKE_SIZE, SNAKE_SPEED, SNAKE_SEPARATION, FPS, KEY, APPLE_SIZE
+
+
+class Segment():
+
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.size = (SNAKE_SIZE, SNAKE_SIZE)
+        self.direction = KEY['RIGHT']
+
+
+class Snake():
+
+    def __init__(self, x, y, head_color, body_color):
+        self.speed = SNAKE_SPEED
+        seg = Segment(x + SNAKE_SEPARATION, y, head_color)
+        self.stack = [seg]
+        next_x = x
+        for i in range(1, 3):
+            next_x -= 10
+            seg = Segment(next_x + SNAKE_SEPARATION, y, body_color)
+            self.stack.append(seg)
+        self.size_x = len(self.stack) * SNAKE_SIZE
+        self.size_y = SNAKE_SIZE
+
+    def head(self):
+        return self.stack[0]
+
+    def move(self):
+        last_element = len(self.stack) - 1
+        while last_element != 0:
+            self.stack[last_element].direction = self.stack[last_element - 1].direction
+            self.stack[last_element].x = self.stack[last_element - 1].x
+            self.stack[last_element].y = self.stack[last_element - 1].y
+            last_element -= 1
+        if len(self.stack) < 2:
+            last_segment = self
+        else:
+            last_segment = self.stack.pop(last_element)
+        last_segment.direction = self.stack[0].direction
+        if self.stack[0].direction == KEY["UP"]:
+            last_segment.y = self.stack[0].y - (SNAKE_SPEED * FPS) - SNAKE_SIZE - SNAKE_SEPARATION
+        elif self.stack[0].direction == KEY["DOWN"]:
+            last_segment.y = self.stack[0].y + (SNAKE_SPEED * FPS) + SNAKE_SIZE + SNAKE_SEPARATION
+        elif self.stack[0].direction == KEY["LEFT"]:
+            last_segment.x = self.stack[0].x - (SNAKE_SPEED * FPS) - SNAKE_SIZE - SNAKE_SEPARATION
+        elif self.stack[0].direction == KEY["RIGHT"]:
+            last_segment.x = self.stack[0].x + (SNAKE_SPEED * FPS) + SNAKE_SIZE + SNAKE_SEPARATION
+        self.stack.insert(0, last_segment)
+
+    def grow(self):
+        last_element = len(self.stack)-1
+        x = self.stack[last_element].x
+        y = self.stack[last_element].y
+        color = self.stack[last_element].color
+        seg = Segment(x + SNAKE_SEPARATION, y, color)
+        self.stack.append(seg)
+
+
+class Apple():
+
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.position = (x, y, APPLE_SIZE, APPLE_SIZE)
